8bc2f31bfbf586ad9c1c196837c7484a2fa64a01
halom <halom@whu-desktop.attlocal.net>
2018-05-05 18:28:48 -0700

    Put code in Player check it!

--- APCSFinal/src/Player.java
+++ APCSFinal/src/Player.java
@@ -1,4 +1,17 @@
+import processing.core.PImage;
 
 public class Player {
-
+	private double xCoord;
+	private double yCoord;
+	private int health;
+	private double width;
+	private double height;
+	private PImage character;
+	
+	public Player(double x, double y) {
+		xCoord = x;
+		yCoord = y;
+		character = loadImage("character.png");
+	}
+	
 }
