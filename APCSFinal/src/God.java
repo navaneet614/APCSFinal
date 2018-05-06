0d9be7278edb7444af6bb4ecb7932947b1729fff
shaunakbhandarkar <shaunakbhandarkar@10.0.0.8>
2018-05-05 18:02:29 -0700

    added God class

---
+++ APCSFinal/src/God.java
@@ -0,0 +1,18 @@
+
+public class God 
+{
+	private ArrayList<Obstacle> godstacles = new ArrayList<Obstacle>();
+	
+	public God() 
+	{
+		
+	}
+	
+	public void throwObstacle( Obstacle ob ) 
+	{
+		
+	}
+	
+	
+	
+}
