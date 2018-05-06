045e2df7423c7b704247ad5acf8e9bd4d1ccfc3c
shaunakbhandarkar <shaunakbhandarkar@10.0.0.8>
2018-05-05 18:45:45 -0700

    gamescreen y'all #TBT!!

--- APCSFinal/src/Main.java
+++ APCSFinal/src/Main.java
@@ -1,9 +1,30 @@
+import java.awt.Color;
+import java.awt.Container;
+import java.awt.Dimension;
+
+import javax.swing.JFrame;
+
+import processing.awt.PSurfaceAWT;
+import processing.core.PApplet;
 
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		GameScreen drawing = new GameScreen();
+		PApplet.runSketch(new String[]{""}, drawing);
+		PSurfaceAWT surf = (PSurfaceAWT) drawing.getSurface();
+		PSurfaceAWT.SmoothCanvas canvas = (PSurfaceAWT.SmoothCanvas) surf.getNative();
+		JFrame window = (JFrame)canvas.getFrame();
+		
+		window.setSize(800, 600);
+		window.setMinimumSize(new Dimension(100,100));
+		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		window.setResizable(true);
+		//Container c = window.getContentPane();
+		//c.setBackground( Color.BLUE );
 
+		window.setVisible(true);
 	}
 
 }
