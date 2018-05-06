c54a07a1cf60e2786197ebecfacfb914296d3d9a
halom <halom@whu-desktop.attlocal.net>
2018-05-05 19:17:43 -0700

    WE GOT WORK IN BOIS

--- APCSFinal/src/GameScreen.java
+++ APCSFinal/src/GameScreen.java
@@ -5,16 +5,18 @@ import processing.core.PApplet;
 public class GameScreen extends PApplet
 {
 	private static final float ORIGINAL_WIDTH = 800, ORIGNAL_HEIGHT = 600;
-	StartMenu startMenu;
+	private StartMenu startMenu;
+	private Player guy ;
 	
 	public GameScreen() 
 	{
 		startMenu = new StartMenu();
+		guy = new Player(50,50,50,50);
 	}
 	
 	public void setup() 
 	{
-		
+		guy.setup(this);
 	}
 	
 	public void draw() 
@@ -22,5 +24,6 @@ public class GameScreen extends PApplet
 		scale(width / ORIGINAL_WIDTH, height / ORIGNAL_HEIGHT);
 		background( Color.WHITE.getRGB() );
 		startMenu.draw(this);
+		guy.draw(this);
 	}
 }
