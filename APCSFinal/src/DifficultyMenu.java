744c4d74821e317fb8858a67c80738077d87ead1
halom <halom@whu-desktop.attlocal.net>
2018-05-13 18:30:42 -0700

    HELP IM BEING KEPT HOSTAGE IF THIS MESSAGE IS RECEIVED HELP!

--- APCSFinal/src/DifficultyMenu.java
+++ APCSFinal/src/DifficultyMenu.java
@@ -1,10 +1,30 @@
+import java.awt.Color;
 
 public class DifficultyMenu extends Menu {
-
-	@Override
+	
+	public DifficultyMenu() {
+		doButtons();
+	}
+	
+	private void doButtons() {
+		this.addButton(new Button(325, 200, 150, 50, "Easy", Color.BLACK, Color.WHITE, Color.LIGHT_GRAY, Color.BLUE));
+		this.addButton(new Button(325, 260, 150, 50, "Not Easy", Color.BLACK, Color.WHITE, Color.LIGHT_GRAY, Color.BLUE));
+		this.addButton(new Button(325, 320, 150, 50, "Really Not Easy", Color.BLACK, Color.WHITE, Color.LIGHT_GRAY, Color.BLUE));
+		this.addButton(new Button(325, 380, 150, 50, "Bacl", Color.BLACK, Color.WHITE, Color.LIGHT_GRAY, Color.BLUE));
+	}
+	
+	
+	
 	public void doButtonAction(String buttonText, GameScreen gameScreen) {
-		// TODO Auto-generated method stub
-
+		if(buttonText.equals("Easy")) {
+			gameScreen.changeMenuMode("level1");
+		} else if(buttonText.equals("Not Easy")) {
+			gameScreen.changeMenuMode("level2"); 
+		}else if(buttonText.equals("Really Not Easy")) {
+			gameScreen.changeMenuMode("level3");
+		} else if(buttonText.equals("Back")) {
+			gameScreen.changeMenuMode("main");
+		}
 	}
 
 }
