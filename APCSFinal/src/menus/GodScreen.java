b44d55e22198ef49c4b5be5bb733f06f1ef32dfd
navaneet614 <navaneet@gmail.com>
2018-05-20 21:56:52 -0700

    BOIs

--- APCSFinal/src/menus/GodScreen.java
+++ APCSFinal/src/menus/GodScreen.java
@@ -1,4 +1,5 @@
 package menus;
+
 import java.awt.Color;
 
 import javax.swing.JDialog;
@@ -18,17 +19,17 @@ import utilities.ImageLoader;
  * 
  * @author William Hu
  * 
- * In multiplayer mode, this is the second user's GUI, which 
- * allows him or her to select and place up to 10 obstacles
- * in the given stage.
+ *         In multiplayer mode, this is the second user's GUI, which allows him
+ *         or her to select and place up to 10 obstacles in the given stage.
  *
  */
 public class GodScreen extends Menu {
 	private float x, y, width, height;
 	private ClickableObstacle spike, glue, turret, mine, block;
 	private God god;
-	private boolean showedPopup, drawSpike, drawGlue, drawTurret, dragging, drawMine, drawBlock,undoMode;
+	private boolean showedPopup, drawSpike, drawGlue, drawTurret, dragging, drawMine, drawBlock, undoMode;
 	private Button done, undo;
+	private int count;
 
 	public GodScreen(float x, float y, float width, float height, God god) {
 		this.x = x;
@@ -38,15 +39,17 @@ public class GodScreen extends Menu {
 		spike = new ClickableObstacle(x, y, width / 10, height, new Spike(x, y, 50, 50), "Spikes");
 		glue = new ClickableObstacle(x + width / 10, y, width / 10, height, new Glue(x, y, 50, 50), "Glue");
 		turret = new ClickableObstacle(x + width / 5, y, width / 10, height, new Turret(x, y, 50, 50, 180), "Turrets");
-		mine = new ClickableObstacle(x + 3*width / 10, y, width / 10, height, new LandMine(x, y, 25, 25), "Mine");
-		block = new ClickableObstacle(x + 2*width/5, y, width / 10, height, new Block(x, y, 50, 50), "Block");
+		mine = new ClickableObstacle(x + 3 * width / 10, y, width / 10, height, new LandMine(x, y, 25, 25), "Mine");
+		block = new ClickableObstacle(x + 2 * width / 5, y, width / 10, height, new Block(x, y, 50, 50), "Block");
 		addButton(spike);
 		addButton(glue);
 		addButton(turret);
 		addButton(mine);
 		addButton(block);
-		done = new Button(x + 5*width / 8, y, width / 8, height, "Done", Color.BLACK, Color.WHITE, Color.LIGHT_GRAY, Color.BLUE);
-		undo = new Button(x+ width/2, y, width/8, height, "Undo", Color.black,Color.WHITE, Color.LIGHT_GRAY, Color.BLUE);
+		done = new Button(x + 5 * width / 8, y, width / 8, height, "Done", Color.BLACK, Color.WHITE, Color.LIGHT_GRAY,
+				Color.BLUE);
+		undo = new Button(x + width / 2, y, width / 8, height, "Undo", Color.black, Color.WHITE, Color.LIGHT_GRAY,
+				Color.BLUE);
 		this.addButton(done);
 		this.addButton(undo);
 		this.god = god;
@@ -55,17 +58,27 @@ public class GodScreen extends Menu {
 	}
 
 	public void draw(PApplet drawer) {
-	    if(!showedPopup) {
-		    JOptionPane optionPane = new JOptionPane("Please make sure the level you create is possible to complete. If you make it impossible, nobody will like you.");
-		    JDialog dialog = optionPane.createDialog("IMPORTANT MESSAGE");
-		    dialog.setAlwaysOnTop(true);
-		    dialog.setVisible(true);
-		    dialog.toFront();
-		    optionPane.setRequestFocusEnabled(true);
-		    optionPane.grabFocus();
-		    optionPane.requestFocus();
-		    showedPopup = true;
-	    }
+		if (!showedPopup) {
+			// JOptionPane optionPane = new JOptionPane("Please make sure the level you
+			// create is possible to complete. If you make it impossible, nobody will like
+			// you.");
+			// JDialog dialog = optionPane.createDialog("IMPORTANT MESSAGE");
+			// dialog.setAlwaysOnTop(true);
+			// dialog.setVisible(true);
+			// dialog.toFront();
+			// optionPane.setRequestFocusEnabled(true);
+			// optionPane.grabFocus();
+			// optionPane.requestFocus();
+			drawer.fill(Color.BLACK.getRGB());
+			drawer.text(
+					"Please make sure the level you create is possible to complete.\n If you make it impossible, nobody will like you.",
+					350, 50);
+			count++;
+			if (count > 360) {
+				count = 0;
+				showedPopup = true;
+			}
+		} else {
 		drawer.fill(Color.LIGHT_GRAY.getRGB());
 		drawer.rect(x, y, width, height);
 		spike.draw(drawer);
@@ -91,21 +104,24 @@ public class GodScreen extends Menu {
 
 		if (dragging) {
 			if (drawSpike) {
-				drawer.image(ImageLoader.spike, (drawer.mouseX / (drawer.width / 800f)), (drawer.mouseY / (drawer.height / 600f)), 50, 40);
+				drawer.image(ImageLoader.spike, (drawer.mouseX / (drawer.width / 800f)),
+						(drawer.mouseY / (drawer.height / 600f)), 50, 40);
 			} else if (drawGlue) {
-				drawer.image(ImageLoader.glue, (drawer.mouseX / (drawer.width / 800f)), (drawer.mouseY / (drawer.height / 600f)), 50, 10);
+				drawer.image(ImageLoader.glue, (drawer.mouseX / (drawer.width / 800f)),
+						(drawer.mouseY / (drawer.height / 600f)), 50, 10);
 			} else if (drawTurret) {
-				drawer.image(ImageLoader.turret, (drawer.mouseX / (drawer.width / 800f)), (drawer.mouseY / (drawer.height / 600f)), 50, 50);
-			}else if(drawMine) {
-				drawer.image(ImageLoader.mine, (drawer.mouseX / (drawer.width / 800f)), (drawer.mouseY / (drawer.height / 600f)), 25, 25);
-			}else if(drawBlock) {
-				drawer.image(ImageLoader.block, (drawer.mouseX / (drawer.width / 800f)), (drawer.mouseY / (drawer.height / 600f)), 50, 50);
+				drawer.image(ImageLoader.turret, (drawer.mouseX / (drawer.width / 800f)),
+						(drawer.mouseY / (drawer.height / 600f)), 50, 50);
+			} else if (drawMine) {
+				drawer.image(ImageLoader.mine, (drawer.mouseX / (drawer.width / 800f)),
+						(drawer.mouseY / (drawer.height / 600f)), 25, 25);
+			} else if (drawBlock) {
+				drawer.image(ImageLoader.block, (drawer.mouseX / (drawer.width / 800f)),
+						(drawer.mouseY / (drawer.height / 600f)), 50, 50);
 			}
 		}
-		
-		
+		}
 	}
-	
 
 	@Override
 	public void doButtonAction(String buttonText, GameScreen gameScreen) {
@@ -130,36 +146,35 @@ public class GodScreen extends Menu {
 			drawMine = false;
 			undoMode = false;
 			drawBlock = false;
-		}else if(buttonText.equals("Mine")) {
+		} else if (buttonText.equals("Mine")) {
 			drawSpike = false;
 			drawGlue = false;
 			drawTurret = false;
 			drawMine = true;
 			drawBlock = false;
 			undoMode = false;
-		}else if(buttonText.equals("Block")) {
+		} else if (buttonText.equals("Block")) {
 			drawBlock = true;
 			drawSpike = false;
 			drawGlue = false;
 			drawTurret = false;
 			drawMine = false;
 			undoMode = false;
-		} else if(buttonText.equals("Done")) {
+		} else if (buttonText.equals("Done")) {
 			drawBlock = true;
 			drawSpike = false;
 			drawGlue = false;
 			drawTurret = false;
 			drawMine = false;
 			undoMode = false;
-			while(god.canPlace()) {
+			while (god.canPlace()) {
 				god.place();
 			}
-		}
-		else if(buttonText.equals("Undo")) {
-			if(god.getPlacedObstacles()>0) {
+		} else if (buttonText.equals("Undo")) {
+			if (god.getPlacedObstacles() > 0) {
 				gameScreen.removeObstacle();
 				gameScreen.setupBlocks();
-			god.subtractPlacedObstacles();
+				god.subtractPlacedObstacles();
 			}
 			drawBlock = false;
 			drawSpike = false;
@@ -177,11 +192,11 @@ public class GodScreen extends Menu {
 			return "spike";
 		} else if (drawGlue)
 			return "glue";
-		else if(drawMine) {
+		else if (drawMine) {
 			return "mine";
-		}else if(drawBlock) {
+		} else if (drawBlock) {
 			return "block";
-		}else if(drawTurret) {
+		} else if (drawTurret) {
 			return "turret";
 		}
 		return "";
