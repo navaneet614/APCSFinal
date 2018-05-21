9ac9d33aacaf55971da77f8dbe456f451a99a1c4
shaunakbhandarkar <sbhandarkar382@student.fuhsd.org>
2018-05-20 22:14:56 -0700

    removing javadoc for now

--- APCSFinal/src/backend/Client.java
+++ APCSFinal/src/backend/Client.java
@@ -4,13 +4,6 @@ import java.net.*;
 
 import java.io.*;
 
-/**
- * 
- * @author shaunakbhandarkar
- * 
- * The Client class 
- *
- */
 public class Client implements AutoCloseable, Runnable {
 	Socket s;
 	private BufferedReader in;
