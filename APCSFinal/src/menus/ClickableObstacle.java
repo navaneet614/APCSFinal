652a79962e72a3130a5ec322e20358c08960cbad
halom <halom@whu-desktop.attlocal.net>
2018-05-20 22:49:02 -0700

    adawdwadawdwa

--- APCSFinal/src/menus/ClickableObstacle.java
+++ APCSFinal/src/menus/ClickableObstacle.java
@@ -2,8 +2,14 @@ package menus;
 import java.awt.Color;
 
 import menus.Button;
+import obstacles.Block;
+import obstacles.Glue;
+import obstacles.LandMine;
 import obstacles.Obstacle;
+import obstacles.Spike;
+import obstacles.Turret;
 import processing.core.PApplet;
+import utilities.ImageLoader;
 
 /**
  * 
@@ -20,6 +26,35 @@ public class ClickableObstacle extends Button {
 	
 	public ClickableObstacle(float x, float y, float width, float height, Obstacle obstacle, String text) {
 		super(x,y,width,height,text, Color.BLACK, Color.WHITE, Color.LIGHT_GRAY, Color.BLUE);
+		this.obstacle = obstacle;
+		this.x = x;
+		this.y = y;
+		this.width = width;
+		this.height = height;
+		this.text = text;
+	}
+	
+	public void draw(PApplet drawer) {
+		drawer.rectMode(PApplet.CORNER);
+		
+		drawer.stroke(Color.BLACK.getRGB());
+		drawer.rect(x, y, width, height);
+		if(obstacle instanceof Spike) {
+		drawer.image(ImageLoader.spike, x+5, y+5,width-15,height-15);
+		}else if (obstacle instanceof Turret) {
+			drawer.image(ImageLoader.turret, x+5, y+5,width-15,height-15);
+		}else if(obstacle instanceof Glue) {
+			drawer.image(ImageLoader.glue, x+5, y+5,width-15,height-15);
+		}else if(obstacle instanceof LandMine) {
+			drawer.image(ImageLoader.mine, x+5, y+5,width-15,height-15);
+		}else if(obstacle instanceof Block) {
+			drawer.image(ImageLoader.block, x+5, y+5,width-15,height-15);
+		}
+	}
+	
+	public ClickableObstacle( float x, float y, float width, float height, Obstacle obstacle, String text, Color fill ) 
+	{
+		super(x,y,width,height,text, Color.BLACK, fill, Color.LIGHT_GRAY, Color.BLUE);
 		this.obstacle = obstacle;
 		this.x = x;
 		this.y = y;
